f1e6bc5fb2209cb31a3d4c06c517e1c38f146e46
lougan <134981472+andyhi93@users.noreply.github.com>
2023-12-25 14:04:09 +0800

    Add files via upload

--- hw38.py
+++ hw38.py
@@ -5,13 +5,13 @@ for i in range(int(n)):
     temp=[int(x) for x in input().split()]
     data[temp[0]]=temp[1::]
     if(temp[1]==0):end.append(temp[0])
-def advantrue(start,end,passed,gold):
-    print(passed,start)
-    if(start in end or start in passed):
+def advantrue(start,end,passed,gold,finalAns)->list:
+    if(start in end or (data.get(start)[1] in passed and data.get(start)[2] in passed)):
         return [gold]
     for i in data.keys():
         if(i not in passed and i in data.get(start)[1::]):
-            ans=advantrue(i, end, passed+[start], gold+data.get(i)[0])
-    return ans
-count=advantrue(start, end, [], data.get(start)[0])
-print(count)
+            ans=advantrue(i, end, passed+[start], gold+data.get(i)[0],finalAns)
+            for j in ans:
+                if j not in finalAns:finalAns.append(j)
+    return finalAns
+print(max(advantrue(start, end, [], data.get(start)[0],[])))
